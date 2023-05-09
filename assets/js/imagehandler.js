f1946d4f05068ef68c07faf217d1311211c85989
Willian Pereira <willianbp88az@gmail.com>
2023-05-09 19:52:12 -0300

    Lets do this

--- assets/js/imagehandler.js
+++ assets/js/imagehandler.js
@@ -1,22 +1,52 @@
-const IMGmodal = document.querySelector('.imgmodal');
-const IMGmodalImage = IMGmodal.querySelector('.modal-image');
-const imageContainers = document.querySelectorAll('.image-container');
+// Shuffle the order of the fanart items
+const fanartSection = document.querySelector('.fanart-section');
+for (let i = fanartSection.children.length; i >= 0; i--) {
+  fanartSection.appendChild(fanartSection.children[Math.random() * i | 0]);
+}
 
+// Open modal on click of fanart item
+const fanartItems = document.querySelectorAll('.fanart-item');
+fanartItems.forEach(item => {
+  const modalId = item.querySelector('img').getAttribute('data-modal');
+  const modal = document.getElementById(modalId);
+  const modalContent = modal.querySelector('.modal-content');
 
-imageContainers.forEach(container => {
-  container.addEventListener('click', () => {
-    const image = container.querySelector('img');
-    const name = container.querySelector('.image-name').textContent;
-    IMGmodalImage.src = image.src;
-    IMGmodalImage.alt = name;
-    IMGmodal.style.display = 'block';
+  item.addEventListener('click', () => {
+    modal.style.display = 'flex';
+    modalContent.scrollTop = 0;
+    document.body.style.overflow = 'hidden';
+
+    // Resize the modal content based on the size of the image
+    const img = modalContent.querySelector('img');
+    const imgWidth = img.width;
+    const imgHeight = img.height;
+    const modalPadding = parseInt(window.getComputedStyle(modalContent).padding) * 2;
+    modalContent.style.width = imgWidth + modalPadding + 'px';
+    modalContent.style.height = imgHeight + modalPadding + 'px';
   });
 });
-IMGmodal.addEventListener('click', () => {
-  IMGmodal.style.display = 'none';
+
+// Close modal on click of close button
+const modalCloseButtons = document.querySelectorAll('.modal-close');
+modalCloseButtons.forEach(button => {
+  const modal = button.parentNode;
+  button.addEventListener('click', () => {
+    modal.style.display = 'none';
+    document.body.style.overflow = 'auto';
+  });
 });
-document.addEventListener('keydown', event => {
-  if (event.key === 'Escape') {
-    IMGmodal.style.display = 'none';
-  }
+
+// Close modal on click outside of modal content
+const modals = document.querySelectorAll('.modal');
+modals.forEach(modal => {
+  const modalContent = modal.querySelector('.modal-content');
+  modal.addEventListener('click', event => {
+    if (event.target === modal) {
+      modal.style.display = 'none';
+      document.body.style.overflow = 'auto';
+    }
+  });
+  modalContent.addEventListener('click', event => {
+    event.stopPropagation();
+  });
 });
