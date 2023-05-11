5b61fe3e272bd726fa095caf56cabfa24a6a65d9
Willian Pereira <62926214+Sty13@users.noreply.github.com>
2023-05-11 17:17:04 -0300

    Update video ids

--- assets/js/youtube.js
+++ assets/js/youtube.js
@@ -1,4 +1,3 @@
-// Define an object that maps div IDs to video IDs
 const videoIds = {
   video1: "Oio7Eksy3MI",
   video2: "Its6d0PBr0A",
@@ -18,26 +17,6 @@ const videoIds = {
   video16: "jcOgOgyGPyE",
   video17: "xAcrr8jJW64",
   video18: "vbC-vb7LpqQ"
-  // video1: "X8avbciUP3c",
-  // video2: "23RpZqv-wuw",
-  // video3: "X8avbciUP3c",
-  // video4: "23RpZqv-wuw",
-  // video5: "X8avbciUP3c",
-  // video6: "23RpZqv-wuw",
-  // video7: "X8avbciUP3c",
-  // video8: "23RpZqv-wuw",
-  // video9: "X8avbciUP3c",
-  // video10: "23RpZqv-wuw",
-  // video11: "X8avbciUP3c",
-  // video12: "X8avbciUP3c",
-  // video13: "X8avbciUP3c",
-  // video14: "X8avbciUP3c",
-  // video15: "X8avbciUP3c",
-  // video16: "X8avbciUP3c",
-  // video17: "X8avbciUP3c",
-  // video18: "X8avbciUP3c",
-
-  // Add more div/video ID mappings as needed
 };
 
 // Define a function that checks which video corresponds to a div
@@ -48,12 +27,10 @@ function checkVideo(divId) {
     console.log(
       `Video ${divId.slice(-1)} setado para a div! Video ID: ${videoId}`
     );
-    // Do something with the video ID, such as embed the video in the div
   } else {
     console.log(`Nenhum vídeo definido para a div ${divId}`);
   }
 }
 
-// Example usage: check which video corresponds to div2
 // Output: "Video 2 setado para a div! Video ID: gthy475tyghs"
 //   https://www.youtube.com/watch?v=
